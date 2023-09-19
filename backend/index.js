83e2d58f9e4b0301b6e8460eb0c51ecd23ddd0ae
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2023-09-19 20:21:25 +0530

    Backend added

---
+++ backend/index.js
@@ -0,0 +1,65 @@
+import('bard-ai').then((module) => {
+  const Bard=module.default;
+    // Now you can use Bard as a module
+const express = require('express');
+var cors = require('cors');
+
+const app = express()
+const port = 5000
+
+app.use(cors())
+app.use(express.json())
+    const COOKIE='bAj726WxO2wcnPVxC0C7_mJVOhIOomaIRGpJvLQkQ9O5Z4G9mA0a31w1Fstf1kyKzeBlpA.'
+
+async function askBard(text) {
+    let myBard = new Bard(COOKIE);
+    text=text;
+    try {
+        let reply=await myBard.ask(text);
+        return reply;
+    } catch (error) {
+        console.log(error);
+    }
+    
+  }
+
+app.post('/api/ask', async (req, res) => {
+    // Perform operations
+    const {text}= req.body;
+    message=await askBard(text);
+    const data = { message: message };
+    res.json(data);
+  });
+import('translate-google').then((module)=>{
+  const translate=module.default;
+  app.post('/api/translate', async (req,res)=>{
+    const {text, code}=req.body;
+    const data=await translateTo(text,code);
+    const obj={data: data}
+    res.json(obj);
+  })
+
+  async function translateTo(text, code)
+  {
+    try {
+      const res = await translate(text, { to: code });
+      return res;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}).catch((error)=>{
+
+})
+
+  const server=app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+  }).catch((error) => {
+    console.error('Error importing bard-ai:', error);
+  });
+
+
+
+
